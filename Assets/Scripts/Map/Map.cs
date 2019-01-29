30875aeb90a9c404e7c18dca8753fa6e76fd28ed
EFSanReis <46160291+EFSanReis@users.noreply.github.com>
2019-01-29 18:43:30 +0300

    Map Bridge Created

--- Assets/Scripts/Map/Map.cs
+++ Assets/Scripts/Map/Map.cs
@@ -1,118 +1,401 @@
-﻿using System.Collections;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class Map : MonoBehaviour
 {
-    private const int minNum = 15;
-    private const int maxNum = 50;
+    #region Constants
 
-    public Tile tileground;
-    public Tile tileground2;
-    public Tile tilewall;
-    public Tile tileDoor;
-    public GameObject ChestObject;
+    private const int MIN_VALUE = 25;
+    private const int MAX_VALUE = 50;
+    private const int PLACE_MAX = 30;
+    private const int MAP_PLACE_MinVALUE = 6;
+    private const int MAP_PLACE_MaxVALUE = 17;
+    private const int MAZE_WIDTH = 3;
 
-    public Tilemap tilemapGround;
-    public Tilemap tilemapWall;
-    private Vector3Int vector3;
+    #endregion
+
+    #region Field
+
+    public List<Vector2Int> PlacesSizeList;
+    public List<Vector3Int> PlacesCoordinatesList;
+    private List<Vector3Int> mBridgeLocation;
+    private List<Vector3Int> mWallLocation;
+
+    private Vector3Int OldTransform;
+    private Vector2Int vector2Int;
     private Vector3Int vector3Int;
 
-    public Map map;
+    public GameObject PlaceObject;
 
-    public int chestCount;
-    public int RandomNumber;
-    private int numberX;
-    private int numberY;
+    public int PlaceCount;
+    private int upDoor;
+    private int downDoor;
+    private int rightDoor;
+    private int leftDoor;
 
-    private Vector2Int size;
-    private Vector3Int[] positions;
-    private TileBase[] tileGroundArray;
-    private TileBase[] tileWallArray;
+    private int TransformX;
+    private int TransformY;
+    private int placeX;
+    private int placeY;
+
+    public List<int> UpDoor;
+    public List<int> DownDoor;
+    public List<int> RightDoor;
+    public List<int> LeftDoor;
+
+    public Tilemap BridgeTilemap;
+    public Tilemap BridgeWallTilemap;
+    public Tile BridgeTile;
+    public Tile BridgeWallTile;
+
+    #endregion
+
+    #region Unity Method
 
     private void Start()
     {
         Init();
     }
 
+    #endregion
+
+    #region Private Methods
+
     private void Init()
     {
-        size = new Vector2Int(EqualNumbers(minNum, maxNum), EqualNumbers(minNum, maxNum));
-        Test();
+        mBridgeLocation = new List<Vector3Int>();
+        mWallLocation = new List<Vector3Int>();
+        PlaceCount = RandomValue(MAP_PLACE_MinVALUE, MAP_PLACE_MaxVALUE);
+        PlacesFind();
+        MapCreate();
     }
 
-    public void Test()
+    private void MapCreate()
     {
-        positions = new Vector3Int[size.x * size.y];
-        tileGroundArray = new TileBase[positions.Length];
-        tileWallArray = new TileBase[positions.Length];
-
-        Vector2Int vector2 = new Vector2Int((int)transform.position.x, (int)transform.position.y);
-
-        for (int x = 0; x < size.y; x++)
+        for (int i = 0; i < PlaceCount; i++)
         {
-            for (int i = 0; i < size.x; i++)
+            if (i != 0)
             {
-                positions[i] = new Vector3Int(vector2.x + i, vector2.y + x, 1);
-                int a = x % 2 == 0 ? 0 : 1;
-                tileGroundArray[i] = i % 2 == a ? tileground : tileground2;
+                UpDoor.Add(upDoor);
+                DownDoor.Add(downDoor);
+                RightDoor.Add(leftDoor);
+                LeftDoor.Add(rightDoor);
             }
-            tilemapGround.SetTiles(positions, tileGroundArray);
-        }
 
-        for (int i = 0; i < size.x; i++)
-        {
-            BorderCreate(i, vector2.x + i, vector2.y + size.y); //Up Border
-            BorderCreate(i, vector2.x + i, vector2.y - 1);     //Down Border
-        }
+            DoorPlacesFind(i);
+            Place place = Instantiate(PlaceObject, PlacesCoordinatesList[i], Quaternion.identity).GetComponent<Place>();
+            place.GetComponent<Place>().MakeEqual(PlacesSizeList[i], PlacesCoordinatesList[i], PlacesCoordinatesList, upDoor, downDoor, rightDoor, leftDoor);
 
-        for (int i = 0; i < size.y; i++)
-        {
-            BorderCreate(i, vector2.x + size.x, vector2.y + i); //Right Border
-            BorderCreate(i, vector2.x - 1, vector2.y + i);    //Left Border
-
-            BorderCreate(i, vector2.x + size.x, vector2.y + size.y);
-            BorderCreate(i, vector2.x + -1, vector2.y + -1);
-            BorderCreate(i, vector2.x - 1, vector2.y + size.y);
-            BorderCreate(i, vector2.x + size.x, vector2.y - 1);
+            if (i != PlaceCount - 1)
+            {
+                BrigeCreate(i);
+            }
         }
     }
 
-    private void BorderCreate(int i, int locationX, int locationY)
+    private void PlacesFind()
     {
-        positions[i] = new Vector3Int(locationX, locationY, 1);
-        tileWallArray[i] = tilewall;
-        tilemapWall.SetTiles(positions, tileWallArray);
-    }
-
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
+        for (int i = 0; i < PlaceCount; i++)
         {
-            Test();
+            TransformX = RandomValue(MIN_VALUE, MAX_VALUE);
+            TransformY = RandomValue(MIN_VALUE, MAX_VALUE);
+            vector2Int = new Vector2Int(TransformX, TransformY);
+            PlacesSizeList.Add(vector2Int);
+            PlaceTransform(i, TransformX, TransformY);
         }
     }
 
-    public int EqualNumbers(int min, int max)
+    private void BrigeCreate(int i)
+    {
+        if (PlacesCoordinatesList[i].y < PlacesCoordinatesList[i + 1].y)
+        {
+            int FirstDoorLocationX = PlacesSizeList[i].x / 6;
+            int SecondDoorLocationX = PlacesSizeList[i + 1].x / 6;
+
+            Vector2Int FirstDoor = new Vector2Int(PlacesCoordinatesList[i].x + FirstDoorLocationX, PlacesCoordinatesList[i].y + PlacesSizeList[i].y + 1);
+            Vector2Int SecondDoor = new Vector2Int(PlacesCoordinatesList[i + 1].x + SecondDoorLocationX, PlacesCoordinatesList[i + 1].y - 1);
+
+            int distanceY = Mathf.Abs(SecondDoor.y - FirstDoor.y);
+
+            if (FirstDoor.x != SecondDoor.x)
+            {
+                for (int x = 0; x < distanceY / 2; x++)
+                {
+                    for (int y = 0; y < MAZE_WIDTH; y++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(FirstDoor.x + y - 1, FirstDoor.y + x, 1));
+                        mBridgeLocation.Add(new Vector3Int(SecondDoor.x + y - 1, SecondDoor.y - x, 1));
+                    }
+                }
+
+                int distanceX = FirstDoor.x > SecondDoor.x ? Mathf.Abs((FirstDoor.x + 2) - (SecondDoor.x - 1)) : Mathf.Abs((SecondDoor.x + 2) - (FirstDoor.x - 1));
+
+                for (int y = 0; y < distanceX; y++)
+                {
+                    int increase = FirstDoor.x > SecondDoor.x ? y * -1 : y;
+                    int transformX = FirstDoor.x > SecondDoor.x ? FirstDoor.x + 1 : FirstDoor.x - 1;
+
+                    for (int a = -1; a < 2; a++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(transformX + increase, FirstDoor.y + distanceY / 2 + a, 1));
+                    }
+                }
+            }
+            else
+            {
+                for (int x = 0; x < distanceY; x++)
+                {
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x + 2, FirstDoor.y + x, 1));
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x - 2, FirstDoor.y + x, 1));
+
+                    for (int y = 0; y < MAZE_WIDTH; y++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(FirstDoor.x + y - 1, FirstDoor.y + x, 1));
+                    }
+                }
+            }
+
+            BorderBridge(true, distanceY, FirstDoor, SecondDoor);
+        }
+
+        if (PlacesCoordinatesList[i].x < PlacesCoordinatesList[i + 1].x)
+        {
+            int FirstDoorLocationY = PlacesSizeList[i].y / 6;
+            int SecondDoorLocationY = PlacesSizeList[i + 1].y / 6;
+
+            Vector2Int FirstDoor = new Vector2Int(PlacesCoordinatesList[i].x + PlacesSizeList[i].x + 1, PlacesCoordinatesList[i].y + FirstDoorLocationY);
+            Vector2Int SecondDoor = new Vector2Int(PlacesCoordinatesList[i + 1].x - 1, PlacesCoordinatesList[i + 1].y + SecondDoorLocationY);
+
+            int distanceX = Mathf.Abs(PlacesCoordinatesList[i + 1].x - FirstDoor.x);
+
+            if (FirstDoor.y != SecondDoor.y)
+            {
+                for (int x = 0; x < distanceX / 2; x++)
+                {
+                    for (int y = 0; y < MAZE_WIDTH; y++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(FirstDoor.x + x, FirstDoor.y + y - 1, 1));
+                        mBridgeLocation.Add(new Vector3Int(SecondDoor.x - x, SecondDoor.y + y - 1, 1));
+                    }
+                }
+
+                int distanceY = FirstDoor.y > SecondDoor.y ? Mathf.Abs((FirstDoor.y + 2) - (SecondDoor.y - 1)) : Mathf.Abs((SecondDoor.y + 2) - (FirstDoor.y - 1));
+
+                for (int y = 0; y < distanceY; y++)
+                {
+                    int increase = FirstDoor.y > SecondDoor.y ? y * -1 : y;
+                    int transformY = FirstDoor.y > SecondDoor.y ? FirstDoor.y + 1 : FirstDoor.y - 1;
+
+                    for (int x = 0; x < MAZE_WIDTH; x++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(FirstDoor.x + distanceX / 2 + x - 2, transformY + increase, 1));
+                    }
+                }
+            }
+            else
+            {
+                for (int x = 0; x < distanceX; x++)
+                {
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x + x, FirstDoor.y + 2, 1));
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x + x, FirstDoor.y - 2, 1));
+
+                    for (int y = 0; y < MAZE_WIDTH; y++)
+                    {
+                        mBridgeLocation.Add(new Vector3Int(FirstDoor.x + x, FirstDoor.y + y - 1, 1));
+                    }
+                }
+            }
+
+            BorderBridge(false, distanceX, FirstDoor, SecondDoor);
+        }
+
+        for (int t = 0; t < mBridgeLocation.Count; t++)        {
+            BridgeTilemap.SetTile(mBridgeLocation[t], BridgeTile);        }
+
+        for (int k = 0; k < mWallLocation.Count; k++)
+        {
+            BridgeWallTilemap.SetTile(mWallLocation[k], BridgeWallTile);
+        }
+    }
+
+    private void BorderBridge(bool updown, int distance, Vector2Int FirstDoor, Vector2Int SecondDoor)
+    {
+        int BorderDistance = (distance / 2) + 2;
+        int meandistance = distance - BorderDistance;
+
+        if (updown)
+        {
+            int gap = Mathf.Abs(FirstDoor.x - SecondDoor.x);
+            int firstDoorTransform = FirstDoor.x > SecondDoor.x ? 2 : -2;
+            int secondDoorTransform = FirstDoor.x > SecondDoor.x ? -2 : 2;
+
+            for (int i = 0; i <= distance; i++)
+            {
+                if (i <= BorderDistance)
+                {
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x + firstDoorTransform, FirstDoor.y + i, 1));
+                    mWallLocation.Add(new Vector3Int(SecondDoor.x + secondDoorTransform, SecondDoor.y - i, 1));
+                }
+
+                if (i == BorderDistance)
+                {
+                    int value = -1;
+
+                    if (FirstDoor.x > SecondDoor.x)
+                    {
+                        for (int x = 0; x < gap; x++)
+                        {
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x + 2 + value, FirstDoor.y + BorderDistance, 1));
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x - 1 + value, SecondDoor.y - BorderDistance, 1));
+                            value--;
+                        }
+                    }
+                    else
+                    {
+                        for (int x = 0; x < gap; x++)
+                        {
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x + 1 - value, FirstDoor.y + meandistance, 1));
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x - 2 - value, SecondDoor.y - meandistance, 1));
+                            value--;
+                        }
+                    }
+                }
+
+                if (i > BorderDistance)
+                {
+                    mWallLocation.Add(new Vector3Int(SecondDoor.x + firstDoorTransform, FirstDoor.y + i, 1));
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x + secondDoorTransform, SecondDoor.y - i, 1));
+                }
+            }
+        }
+        else
+        {
+            int gap = Mathf.Abs(FirstDoor.y - SecondDoor.y);
+            int rightDoorTransform = FirstDoor.y > SecondDoor.y ? 2 : -2;
+            int leftDoorTransform = FirstDoor.y > SecondDoor.y ? -2 : 2;
+
+            for (int i = 0; i <= distance; i++)
+            {
+                if (i <= BorderDistance)
+                {
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x - 1 + i, FirstDoor.y + rightDoorTransform, 1));
+                    mWallLocation.Add(new Vector3Int(SecondDoor.x - i, SecondDoor.y + leftDoorTransform, 1));
+                }
+
+                if (i == BorderDistance)
+                {
+                    int value = -2;
+
+                    if (FirstDoor.y > SecondDoor.y)
+                    {
+                        for (int y = 0; y < gap; y++)
+                        {
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x + BorderDistance - 1, SecondDoor.y - value, 1));
+                            mWallLocation.Add(new Vector3Int(SecondDoor.x - BorderDistance, FirstDoor.y + value, 1));
+                            value--;
+                        }
+                    }
+                    else
+                    {
+                        for (int y = 0; y < gap; y++)
+                        {
+                            mWallLocation.Add(new Vector3Int(FirstDoor.x + BorderDistance - 1, SecondDoor.y + value, 1));
+                            mWallLocation.Add(new Vector3Int(SecondDoor.x - BorderDistance, FirstDoor.y - value, 1));
+                            value--;
+                        }
+                    }
+                }
+
+                if (i > BorderDistance)
+                {
+                    mWallLocation.Add(new Vector3Int(SecondDoor.x - i, FirstDoor.y + leftDoorTransform, 1));
+                    mWallLocation.Add(new Vector3Int(FirstDoor.x - 1 + i, SecondDoor.y + rightDoorTransform, 1));
+                }
+            }
+        }
+    }
+
+    private void PlaceTransform(int i, int transformX, int transformY)
+    {
+        int up = 1;
+        int down = 1;
+        int right = 1;
+        int left = 1;
+        bool isupdown = (Random.value < 0.5f);
+        bool ispositive = (Random.value < 0.5f);
+
+        if (isupdown)
+        {
+            if (ispositive)
+            {
+                down++;
+                placeY += (transformY + PLACE_MAX) * up;
+            }
+            else
+            {
+                up++;
+                placeY -= (-transformY - PLACE_MAX) * down;
+            }
+        }
+        else
+        {
+            if (ispositive)
+            {
+                left++;
+                placeX += (transformX + PLACE_MAX) * right;
+            }
+            else
+            {
+                right++;
+                placeX -= (transformX + PLACE_MAX) * -left;
+            }
+        }
+
+        if (i == 0)
+        {
+            placeX = 0;
+            placeY = 0;
+            vector3Int = new Vector3Int(0, 0, 1);
+        }
+        else
+        {
+            vector3Int = new Vector3Int(placeX, placeY, 1);
+        }
+        PlacesCoordinatesList.Add(vector3Int);
+    }
+
+    private void DoorPlacesFind(int i)
+    {
+        upDoor = 0;
+        downDoor = 0;
+        rightDoor = 0;
+        leftDoor = 0;
+
+        if (i == 0)
+        {
+            downDoor = PlacesCoordinatesList[i + 1].y < PlacesCoordinatesList[i].y ? MAZE_WIDTH : 0;
+            upDoor = PlacesCoordinatesList[i + 1].y > PlacesCoordinatesList[i].y ? MAZE_WIDTH : 0;
+            leftDoor = PlacesCoordinatesList[i + 1].x < PlacesCoordinatesList[i].x ? MAZE_WIDTH : 0;
+            rightDoor = PlacesCoordinatesList[i + 1].x > PlacesCoordinatesList[i].x ? MAZE_WIDTH : 0;
+        }
+        else
+        {
+            OldTransform = i == PlaceCount - 1 ? PlacesCoordinatesList[i - 1] : PlacesCoordinatesList[i + 1];
+
+            downDoor = PlacesCoordinatesList[i - 1].y < PlacesCoordinatesList[i].y && PlacesCoordinatesList[i - 1].y != PlacesCoordinatesList[i].y ? MAZE_WIDTH : 0;
+            upDoor = OldTransform.y > PlacesCoordinatesList[i].y && OldTransform.y != PlacesCoordinatesList[i].y ? MAZE_WIDTH : 0;
+            leftDoor = PlacesCoordinatesList[i - 1].x < PlacesCoordinatesList[i].x && PlacesCoordinatesList[i - 1].x != PlacesCoordinatesList[i].x ? MAZE_WIDTH : 0;
+            rightDoor = OldTransform.x > PlacesCoordinatesList[i].x && OldTransform.x != PlacesCoordinatesList[i].x ? MAZE_WIDTH : 0;
+        }
+    }
+
+    private int RandomValue(int min, int max)
     {
         int number = Random.Range(min, max);
         return number;
     }
 
-    private Vector3 RandomLocationFind(Vector2Int current)
-    {
-        numberX = EqualNumbers(-RandomNumber + 2, RandomNumber - 2);
-        numberY = EqualNumbers(-RandomNumber + 2, RandomNumber - 2);
-
-        Vector3 location = new Vector3(size.x - numberX, size.y - numberY);
-        return location;
-    }
-
-    private void ChestCreate(int ChestCount)
-    {
-        for (int i = 0; i < ChestCount; i++)
-        {
-            Instantiate(ChestObject, RandomLocationFind(size), Quaternion.identity);
-        }
-    }
+    #endregion
 }
---
+++ Assets/Scripts/Map/Place.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Place : MonoBehaviour
+{
+    #region Constants
+
+    private const string TAG_TILEMAP_WALL = "wall";
+    private const string TAG_TILEMAP_GROUND = "Tilemap_Ground";
+    private const int CHEST_WIDHT = 4;
+    private const int DOOR_WIDHT = 3;
+
+    #endregion
+
+    #region Fields
+
+    private TileBase[] tileWallArray;
+    private TileBase[] tileGroundArray;
+    public Tilemap tilemapGround;
+    public Tilemap tilemapWall;
+    public Tile tileground;
+    public Tile tileground2;
+    public Tile tilewall;
+
+    public GameObject ChestObject;
+
+    public Vector2Int size;
+    private Vector3Int vector3;
+    private Vector3Int[] positions;
+
+    public List<Vector3Int> DoorLocations;
+
+    public int ZombieCount;
+    public int EnemyCount;
+    public int TowerExplodCount;
+    public int TowerModeratorCount;
+    public int TowerStandartCount;
+    public int ChestCount;
+    public int BoxCount;
+
+    public int UpDoorCount;
+    public int DownDoorCount;
+    public int RightDoorCount;
+    public int LeftDoorCount;
+
+    public List<int> DoorUpDownPlace;
+    public List<int> DoorUpPlace;
+    public List<int> DoorDownPlace;
+    public List<int> DoorRightLeftPlace;
+    public List<int> DoorRightPlace;
+    public List<int> DoorLeftPlace;
+
+    #endregion
+
+    #region Unity Methods
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    private void Init()
+    {
+        tilemapGround = GameObject.FindWithTag(TAG_TILEMAP_GROUND).GetComponent<Tilemap>();
+        tilemapWall = GameObject.FindWithTag(TAG_TILEMAP_WALL).GetComponent<Tilemap>();
+        PlaceCreate();
+    }
+
+    private void PlaceCreate()
+    {
+        positions = new Vector3Int[size.x * size.y];
+        tileGroundArray = new TileBase[positions.Length];
+        tileWallArray = new TileBase[positions.Length];
+
+        vector3 = new Vector3Int((int)transform.position.x, (int)transform.position.y, 1);
+
+        for (int x = 0; x < size.y; x++)
+        {
+            for (int i = 0; i < size.x; i++)
+            {
+                positions[i] = new Vector3Int(vector3.x + i, vector3.y + x, 1);
+                int a = x % 2 == 0 ? 0 : 1;
+                tileGroundArray[i] = i % 2 == a ? tileground : tileground2;
+            }
+            tilemapGround.SetTiles(positions, tileGroundArray);
+        }
+        // ObjectCreate(ChestObject,1,6,2);
+        BorderCreate();
+    }
+
+    private void BorderCreate()
+    {
+        LocationSetDoor(UpDoorCount, size.x, true, 0);
+        LocationSetDoor(DownDoorCount, size.x, true, -size.y - 1);
+        LocationSetDoor(RightDoorCount, size.y, false, 0);
+        LocationSetDoor(LeftDoorCount, size.y, false, -size.x - 1);
+
+        WallCreate(0, UpDoorCount, size.x, DoorUpPlace, DoorUpDownPlace, size.y, true);
+        WallCreate(UpDoorCount, DownDoorCount, size.x, DoorDownPlace, DoorUpDownPlace, -1, true);
+        WallCreate(0, RightDoorCount, size.y, DoorRightPlace, DoorRightLeftPlace, size.x, false);
+        WallCreate(RightDoorCount, LeftDoorCount, size.y, DoorLeftPlace, DoorRightLeftPlace, -1, false);
+    }
+
+    private void LocationSetDoor(int count, int span, bool isLocationX, int number)
+    {
+        if (count > 0)
+        {
+            int Department = span / count;
+            int Location = Department / 2;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (isLocationX == false)
+                {
+                    for (int x = 0; x < DOOR_WIDHT; x++)
+                    {
+                        BorderDraw(i, vector3.x + size.x + number, vector3.y + Location + x - 1, tileGroundArray, tilemapGround, tileground);
+                        DoorRightLeftPlace.Add(Location + x);
+                    }
+                }
+                else
+                {
+                    for (int x = 0; x < DOOR_WIDHT; x++)
+                    {
+                        BorderDraw(i, vector3.x + Location + x - 1, vector3.y + size.y + number, tileGroundArray, tilemapGround, tileground);
+                        DoorUpDownPlace.Add(Location + x);
+                    }
+                }
+            }
+        }
+    }
+
+    private void WallCreate(int startvalue, int finishvalue, int bordervalue, List<int> array, List<int> doorarray, int transformY, bool isupdown)
+    {
+        for (int i = startvalue; i < finishvalue + startvalue; i++)
+        {
+            array.Add(doorarray[i]);
+        }
+
+        for (int i = 0; i < bordervalue + 2; i++)
+        {
+            if (array.Contains(i) == false)
+            {
+                if (isupdown == false)
+                {
+                    BorderDraw(i, vector3.x + transformY, vector3.y + i - 1, tileWallArray, tilemapWall, tilewall);
+                }
+                else
+                {
+                    BorderDraw(i, vector3.x + i - 1, vector3.y + transformY, tileWallArray, tilemapWall, tilewall);
+                }
+            }
+        }
+    }
+
+    private void BorderDraw(int i, int locationX, int locationY, TileBase[] tilebase, Tilemap tilemap, Tile tile)
+    {
+        positions[i] = new Vector3Int(locationX, locationY, 1);
+        tilebase[i] = tile;
+        tilemap.SetTiles(positions, tilebase);
+    }
+
+    private void ObjectCreate(GameObject createobje, int minvalue, int maxvalue, int objewidth)
+    {
+        int count = RandomValue(minvalue, maxvalue);
+
+        for (int i = 0; i < count; i++)
+        {
+            Instantiate(createobje, RandomLocationFind(objewidth), Quaternion.identity);
+        }
+    }
+
+    private Vector3 RandomLocationFind(int objewidth)
+    {
+        int transformX = RandomValue(objewidth, size.x - objewidth);
+        int transformY = RandomValue(objewidth, size.y - objewidth);
+
+        Vector3 location = new Vector3(transform.position.x + transformX, transform.position.y + transformY);
+        return location;
+    }
+
+    private int RandomValue(int min, int max)
+    {
+        int randomNamber = Random.Range(min, max);
+        return randomNamber;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void MakeEqual(Vector2Int _size, Vector3Int _vector3, List<Vector3Int> vector3list, int up, int down, int right, int left)
+    {
+        UpDoorCount = up;
+        DownDoorCount = down;
+        RightDoorCount = right;
+        LeftDoorCount = left;
+        size = _size;
+        vector3 = _vector3;
+    }
+
+    #endregion
+}
